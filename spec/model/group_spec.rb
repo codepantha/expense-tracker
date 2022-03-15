791d695b41d4f13766d7d20ab480df222c51758a
codepanther <pezechigozirim@gmail.com>
2022-03-16 00:01:06 +0100

    More Group model tess

--- spec/model/group_spec.rb
+++ spec/model/group_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe Group, type: :model do
   let(:user) do
     User.create!(name: 'Rudolf', email: 'rudolf@gmail.com', password: '123456')
   end
-  let(:group) do
-    Group.create!(user: user, name: 'Shoes', icon: '^^')
-  end
 
   subject { Group.new(user: user, name: 'Wears', icon: '(^-)') }
 
-  before { subject.save }
+  before do
+    Group.create!(user: user, name: 'Shoes', icon: '(^-)')
+    subject.save 
+  end
 
   it 'has accurate number of groups' do
-    expect(Group.count).to eq(1)
+    expect(Group.count).to eq(2)
   end
 
   it 'name field must not be empty' do
@@ -24,5 +24,13 @@ RSpec.describe Group, type: :model do
   it 'icon field must not be empty' do
     subject.icon = nil
     expect(subject).to_not be_valid
+  end
+
+  it 'user has 2 groups' do
+    expect(user.groups.count).to eq(2)
+  end
+
+  it 'group belongs to user' do
+    expect(subject.user).to be(user)
   end
 end
