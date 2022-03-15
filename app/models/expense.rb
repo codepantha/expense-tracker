e76fa3e6b24b9f76d9087d0722ab2af44320933c
codepanther <pezechigozirim@gmail.com>
2022-03-15 19:44:15 +0100

    Add model relationships

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,5 @@
 class Expense < ApplicationRecord
+  belongs_to :user
+  has_many :expense_groups
+  has_many :groups, through: :expense_groups
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,5 @@
 class Group < ApplicationRecord
+  belongs_to :user
+  has_many :expense_groups
+  has_many :expenses, through: :expense_groups
 end
