60203cfe8e3c5b524652f9592c2d34d3205ab87b
codepanther <pezechigozirim@gmail.com>
2022-03-15 19:39:51 +0100

    Add user_id column to groups table

---
+++ db/migrate/20220315183513_add_user_id_column_to_groups.rb
@@ -0,0 +1,7 @@
+class AddUserIdColumnToGroups < ActiveRecord::Migration[7.0]
+  def change
+    add_column :groups, :user_id, :integer
+    add_index :groups, :user_id
+    add_foreign_key :groups, :users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_15_183141) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_15_183513) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_15_183141) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,4 +57,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_15_183141) do
   add_foreign_key "expense_groups", "expenses"
   add_foreign_key "expense_groups", "groups"
   add_foreign_key "expenses", "users", column: "author_id"
+  add_foreign_key "groups", "users"
 end
