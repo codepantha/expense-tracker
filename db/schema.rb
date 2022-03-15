77e0e4655f74f0ff60a76bded47a020140ab31aa
codepanther <pezechigozirim@gmail.com>
2022-03-15 19:34:23 +0100

    Create expense_groups join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_15_173935) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_15_183141) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "expense_groups", force: :cascade do |t|
+    t.bigint "expense_id", null: false
+    t.bigint "group_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["expense_id"], name: "index_expense_groups_on_expense_id"
+    t.index ["group_id"], name: "index_expense_groups_on_group_id"
+  end
+
+  create_table "expenses", force: :cascade do |t|
+    t.bigint "author_id"
+    t.string "name"
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_expenses_on_author_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -27,4 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_15_173935) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "expense_groups", "expenses"
+  add_foreign_key "expense_groups", "groups"
+  add_foreign_key "expenses", "users", column: "author_id"
 end
