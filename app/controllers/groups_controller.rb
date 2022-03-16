4b4fb62e9e1bf71b963d6bc6b74586ce4e3c49f0
codepanther <pezechigozirim@gmail.com>
2022-03-16 18:16:53 +0100

    Create add new group functionality

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,15 +1,25 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:show]
   def index
-    @groups = Group.all
+    @groups = current_user.groups
   end
 
   def show
   end
 
   def new
+    @group = Group.new
   end
 
   def create
+    @group = Group.new(group_params)
+    @group.user = current_user
+
+    if @group.save
+      redirect_to groups_path, notice: "#{@group.name} added successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
