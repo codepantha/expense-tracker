3af2cc8b3937442e45ac059e9cf5466c51b199c3
codepanther <pezechigozirim@gmail.com>
2022-03-18 23:32:31 +0100

    Add expenses request tests

--- spec/requests/expenses_spec.rb
+++ spec/requests/expenses_spec.rb
@@ -1,24 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Expenses', type: :request do
+  before :each do
+    @user = User.create(name: 'Prom', email: 'email@test.com', password: '123456')
+    @group = Group.create!(user: @user, name: 'game', icon: 'game_icon')
+    @expense = @group.expenses.create(user: @user, name: 'Legend of the kage', amount: 10)
+  end
+
   describe 'GET /index' do
     it 'returns http success' do
-      get '/expenses/index'
-      expect(response).to have_http_status(:success)
+      get group_expenses_path(@group)
+      expect(response).to have_http_status(302)
     end
   end
 
   describe 'GET /new' do
     it 'returns http success' do
-      get '/expenses/new'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET /create' do
-    it 'returns http success' do
-      get '/expenses/create'
-      expect(response).to have_http_status(:success)
+      get new_group_expense_path(@group)
+      expect(response).to have_http_status(302)
     end
   end
 end
